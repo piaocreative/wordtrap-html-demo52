fb1c57b695a4db18c91da40b4b6838296915e7b3
piaocreative <johnpiao171019@gmail.com>
2021-09-11 04:06:31 +0800

    demo52-0911

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 $( document ).ready( function() {
 
     $('.banners').slick({
-        dots: false,
+        dots: true,
         infinite: true,
         speed: 300,
         slidesToShow: 1,
         adaptiveHeight: true,
-        arrows:true
+        arrows:true,
+    });
+
+    var $slider = $('.banners');
+
+    $slider.on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
+        var i = (currentSlide ? currentSlide : 0) + 1;
+        $(".active").text("0"+i);
+        $(".total").html("0"+slick.slideCount)
     });
 
     $('.posts').slick({
@@ -41,13 +49,88 @@ $( document ).ready( function() {
       ]
   });
 
-  $('.testimonials').slick({
+  $('.teams').slick({
+    dots: false,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+});
+
+$(".team-prev").click(function(e) {
+  e.preventDefault();
+  $('.teams').slick('slickPrev');
+})
+
+$(".team-next").click(function(e) {
+  e.preventDefault();
+  $('.teams').slick('slickNext');
+})
+
+  $('.testimonials-slick').slick({
     dots: false,
     infinite: true,
     speed: 300,
     slidesToShow: 1,
-    adaptiveHeight: true,
-    arrows:true
+    arrows:false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
+
+  $(".testimonial-prev").click(function(e) {
+    e.preventDefault();
+    $('.testimonials-slick').slick('slickPrev');
+  })
+  
+  $(".testimonial-next").click(function(e) {
+    e.preventDefault();
+    $('.testimonials-slick').slick('slickNext');
+  })
 
 });
